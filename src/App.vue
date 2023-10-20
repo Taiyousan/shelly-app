7e9cc76848d029ec64251f1c2bb07e95f82649c1
Shams Benhamou <shamsbenhamou@gmail.com>
2023-10-20 10:28:38 +0200

    feat: add first api interface

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <p>Template vue</p>
+  <div>
+    <nav class="navbar">
+      <ul>
+        <li><RouterLink to="/">Accueil</RouterLink></li>
+        <li><RouterLink to="/about">À propos</RouterLink></li>
+        <li><RouterLink to="/contact">Contact</RouterLink></li>
+      </ul>
+    </nav>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+
+const apiData = ref([]);
+
+onMounted(async () => {
+  const response = await fetch("http://192.168.1.100/shelly");
+  apiData.value = await response.json();
+  console.log(apiData.value);
+});
+</script>
 
 <template>
-  <p>HomeView</p>
+  <div class="api-results">
+    <h2>Résultats de l'API</h2>
+
+    <!-- Affichage des clés et valeurs du résultat -->
+    <ul>
+      <li v-for="(value, key) in apiData" :key="key">
+        <strong>{{ key }}:</strong> {{ value }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
