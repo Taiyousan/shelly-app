fc070bb2b235f98099653c3a3d799eee384c4879
Shams Benhamou <shamsbenhamou@gmail.com>
2023-10-20 12:25:23 +0200

    wip: on off cloud request

--- src/views/ApiView.vue
+++ src/views/ApiView.vue
@@ -5,6 +5,7 @@ import axios from "axios";
 const apiData = ref([]);
 let displayData = ref(false);
 let onOffToggle = ref(false); // true = on
+let isOn = ref(false);
 
 onMounted(async () => {
   const response = await axios.post(
@@ -21,8 +22,30 @@ onMounted(async () => {
     }
   );
   apiData.value = await response.data.data.device_status;
+  onOffToggle.value = await response.data.data.device_status.relays[0].ison;
   console.log(toRaw(response));
 });
+
+// function async
+async function switchPower(param) {
+  console.log(param);
+  const response = await axios.post(
+    `"https://shelly-86-eu.shelly.cloud/relay/0?turn=${param}"`,
+    {
+      id: "80646F827174",
+      auth_key:
+        "MWRmYzM2dWlkE62C6C4C76F817CE0A3D2902F5B5D4C115E49B28CF8539114D9246505DE5D368D560D06020A92480",
+    },
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  apiData.value = await response.data.data.device_status;
+  onOffToggle.value = await response.data.data.device_status.relays[0].ison;
+  console.log(toRaw(response));
+}
 </script>
 
 <template>
@@ -49,7 +72,13 @@ onMounted(async () => {
       <div
         class="on-off-toggle"
         :class="{ 'on-toggle': onOffToggle, 'off-toggle': !onOffToggle }"
-        @click="onOffToggle = !onOffToggle"
+        @click="
+          () => {
+            onOffToggle = !onOffToggle;
+            const param = onOffToggle ? 'on' : 'off';
+            switchPower(param);
+          }
+        "
       >
         {{ onOffToggle ? "ON" : "OFF" }}
       </div>
